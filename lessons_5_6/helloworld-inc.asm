0cb491d0848545d2c07dfeba027e161c0ddc52b4
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-12-14 20:12:49 -0800

    lesson 7

--- lessons_5_6/helloworld-inc.asm
+++ lessons_5_6/helloworld-inc.asm
@@ -6,8 +6,9 @@
 %include	'functions.asm'
 
 SECTION .data
-msg1	db	'Hello, brave new world!', 0Ah
-msg2	db	'This is how we recycle in NASM.', 0Ah
+msg1	db	'Hello, brave new world!', 0Ah, 0h
+msg2	db	'This is how we recycle in NASM.', 0Ah, 0h
+msg3	db	'abcd', 0Ah, 0h
 
 SECTION .text
 global	_start
@@ -18,5 +19,8 @@ _start:
 
 	mov	eax, msg2	; move the address of our second message string into EAX
 	call	sprint		; call our string printing function
+	
+	mov	eax, msg3
+	call	sprint	
 
 	call	quit		; call our quit function
